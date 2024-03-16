1c40df4d8e25fb7ad96e8f68b78bd18097f9e080
erieger11 <erieger530@gmail.com>
2024-03-15 21:04:06 -0400

    finished

--- src/test/java/rocks/zipcodewilmington/DogHouseTest.java
+++ src/test/java/rocks/zipcodewilmington/DogHouseTest.java
@@ -1,8 +1,11 @@
 package rocks.zipcodewilmington;
 
+import org.junit.Assert;
 import org.junit.Test;
+import rocks.zipcodewilmington.animals.Cat;
 import rocks.zipcodewilmington.animals.Dog;
 import rocks.zipcodewilmington.animals.animal_creation.AnimalFactory;
+import rocks.zipcodewilmington.animals.animal_storage.CatHouse;
 import rocks.zipcodewilmington.animals.animal_storage.DogHouse;
 
 import java.util.Date;
@@ -12,9 +15,64 @@ import java.util.Date;
  */
 public class DogHouseTest {
     // TODO - Create tests for `void add(Dog dog)`
+    @Test
+    public void addDogTest(){
+        Dog dog = new Dog(null,null,null);
+
+        DogHouse.add(dog);
+
+        int actual =  DogHouse.getNumberOfDogs();
+
+
+        int expected = 1;
+        Assert.assertEquals(expected,actual);
+    }
     // TODO - Create tests for `void remove(Integer id)`
+    @Test
+    public void removeDogId(){
+        Dog dog = new Dog(null,null,981);
+
+        DogHouse.add(dog);
+
+        DogHouse.remove(981);
+
+        int actual = DogHouse.getNumberOfDogs();
+
+        int expected = 0;
+
+        Assert.assertEquals(expected,actual);
+    }
     // TODO - Create tests for `void remove(Dog dog)`
+    @Test
+    public void removeDog(){
+        Dog dog = new Dog("Turbo",null,null);
+
+        DogHouse.add(dog);
+
+        DogHouse.remove(dog);
+
+        int actual = DogHouse.getNumberOfDogs();
+
+        int expected = 0;
+
+        Assert.assertEquals(expected,actual);
+    }
+
     // TODO - Create tests for `Dog getDogById(Integer id)`
+    @Test
+    public void getDogById(){
+        Dog dog = new Dog(null,null,981);
+
+        DogHouse.add(dog);
+
+        DogHouse.getDogById(981);
+
+        Dog actual = DogHouse.getDogById(981);
+
+        Dog expected = dog;
+
+        Assert.assertEquals(expected,actual);
+    }
     // TODO - Create tests for `Integer getNumberOfDogs()`
 
     @Test
